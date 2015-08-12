47dcbbb09824dc13d2c198a9e9c64ee0ef9bd894
ganxiangdong <phpwww@126.com>
2015-08-12 09:02:13 +0800

    添加db配置类

---
+++ config/Db.php
@@ -0,0 +1,31 @@
+<?php
+namespace Config;
+
+/**
+ * 数据库配置
+ * 程序会一次生成一堆连接，放入到连接池中，\Config\Server中配置的
+ * 如果有多个，则会按闲忙程序自动分发
+ * Class Db
+ * @package Config
+ */
+class Db
+{
+    /**
+     * mysql配置
+     * @var array
+     */
+    public static $mysql = array(
+        'read' => [
+            '192.168.1.1',
+        ],
+        'write' => [
+            '196.168.1.2'
+        ],
+        'database'  => 'database',
+        'username'  => 'root',
+        'password'  => '',
+        'charset'   => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix'    => '',
+    );
+}

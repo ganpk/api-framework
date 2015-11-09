b5f1b1a27c9e2392971115f6a5d488cf15becb43
甘向东 <ganxiangdong@milanoo.com>
2015-11-09 20:28:05 +0800

    测试基类中添加调用API方法

--- app/test/BaseTest.php
+++ app/test/BaseTest.php
@@ -7,4 +7,21 @@ require '../../framework/libs/Test.php';
  */
 abstract class BaseTest extends \Framework\Libs\Test
 {
+    /**
+     * 调用API接口
+     * @param unknown $class
+     * @param unknown $method
+     * @param array $post
+     * @param array $header
+     * @return array
+     */
+    public function callApi($class, $method, $post = array(), $header = array())
+    {
+        $classPath = '\App\Apis\\'.$class;
+        $obj = new $classPath();
+        $obj->params = $post;
+        $data = $obj->{$method}();
+        $data = \Framework\Libs\Utility::converToHump($data);
+        return $data;
+    }
 }

237f377104645e6df9d93ce5db72e7c695bff33e
甘向东 <ganxiangdong@milanoo.com>
2015-11-10 11:50:56 +0800

    生成API文件命令行添加两个参数 第一个文档存在目录 第二个文档文件名

--- framework/bin/build_document.php
+++ framework/bin/build_document.php
@@ -1,4 +1,22 @@
 <?php
+//存放目录
+if (!empty($argv[1])) {
+    $dir = $argv[1];
+    if (!is_dir($dir)) {
+        exit('not found the directory : '.$dir.PHP_EOL);
+    }
+} else {
+    $dir = FRAMEWORK_PATH.'/bin';
+}
+
+//文件名
+if (!empty($argv[2])) {
+    $fileName = $argv[2];
+} else {
+    $fileName = 'documents.html';
+}
+$filePath = "{$dir}/{$fileName}";
+
 //定义框架路径
 define('FRAMEWORK_PATH', dirname(__DIR__));
 //定义应用路径
@@ -6,5 +24,12 @@ define('APP_PATH', dirname(FRAMEWORK_PATH).'/app');
 require(FRAMEWORK_PATH."/vendor/autoload.php");
 $apiPath = dirname(dirname(__DIR__)).'/app/apis';
 $swagger = \Swagger\scan($apiPath);
-file_put_contents('documents.html',$swagger);
+
+$res = file_put_contents($filePath,$swagger);
+
+if ($res) {
+    exit("【success】{$filePath}".PHP_EOL);
+} else {
+    exit("【fail】 please check the directory's permission".PHP_EOL);
+}
 

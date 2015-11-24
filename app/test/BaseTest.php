0650a9b10ce88f1a83a5726e6d9efb24f7805bb3
甘向东 <ganxiangdong@milanoo.com>
2015-11-24 11:04:32 +0800

    test基类返回格式修复为和api一致

--- app/test/BaseTest.php
+++ app/test/BaseTest.php
@@ -13,15 +13,22 @@ abstract class BaseTest extends \Framework\Libs\Test
      * @param unknown $method
      * @param array $post
      * @param array $header
+     * @param string  $dataType json | array
      * @return array
      */
-    public function callApi($class, $method, $post = array(), $header = array())
+    public function callApi($class, $method, $post = array(), $header = array(), $dataType = 'array')
     {
         $classPath = '\App\Apis\\'.$class;
         $obj = new $classPath();
         $obj->params = $post;
         $data = $obj->{$method}();
         $data = \Framework\Libs\Utility::converToHump($data);
+        $data = \Framework\Libs\Utility::getOutputData($data['codeArr'], $data['result'], $data['extData']);
+        if ($dataType == 'json') {
+            return;
+        } else {
+            $data = json_decode($data, true);
+        }
         return $data;
     }
 }

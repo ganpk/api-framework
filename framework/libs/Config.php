30a9845081f221416bbee8c3cf3aaf8d77534ee5
甘向东 <ganxiangdong@milanoo.com>
2015-11-27 10:49:20 +0800

    当环境覆盖级配置文件名统一用小写

--- framework/libs/Config.php
+++ framework/libs/Config.php
@@ -22,21 +22,17 @@ abstract class Config
     public static function instance()
     {
         $class = get_called_class();
-        if (empty(self::$instances[$class])) {
+        $classKey = $class;
+        if (!isset(self::$instances[$classKey])) {
             //实例化子类
-            self::$instances[$class] = new $class();
+            self::$instances[$classKey] = new $class();
 
             //获取当前环境配置文件路径
             $class = ltrim($class, '\\');
             $classPathArr = explode('\\', $class);
             $configClassName = array_pop($classPathArr);
-            array_shift($classPathArr);
-            $filePath = APP_PATH;
-            foreach ($classPathArr as $v) {
-                $filePath .= '/'.strtolower($v);
-            }
-            $filePath .= '/'.RUN_MOD."/{$configClassName}.php";
-
+            $configClassName = strtolower($configClassName);
+            $filePath = APP_PATH.'/config/'.RUN_MOD."/{$configClassName}.php";
             //如果存在指定环境的配置文件，则使用环境配置项覆盖全局配置项
             if (file_exists($filePath)) {
                 //存在指定环境配置文件
@@ -44,11 +40,11 @@ abstract class Config
                 //使用环境配置项覆盖全局配置项
                 if (!empty($configs) && is_array($configs)) {
                     foreach ($configs as $k => $v) {
-                        self::$instances[$class]->{$k} = $v;
+                        self::$instances[$classKey]->{$k} = $v;
                     }
                 }
             }
         }
-        return self::$instances[$class];
+        return self::$instances[$classKey];
     }
 }

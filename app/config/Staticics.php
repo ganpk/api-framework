99b41b85dc4623f83b8cfc5ab4378866415636b4
甘向东 <ganxiangdong@milanoo.com>
2015-11-27 11:00:21 +0800

    fix 上报地址配置默认读取不到

--- app/config/Staticics.php
+++ app/config/Staticics.php
@@ -4,11 +4,12 @@ namespace App\Config;
 /**
  * 上报统计中心配置
  */
-class Staticics
+class Staticics extends \Framework\Libs\Config
 {
     /**
      * 上报地址，为空表示不开启上报
+     * @example udp://127.0.0.1:55656
      * @var string
      */
-    public static $reportAddr = 'udp://127.0.0.1:55656';
+    public $reportAddr = '';
 }

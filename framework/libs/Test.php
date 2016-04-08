f5ce283aa7e263fedb8695c55341b8d8b65ea946
ganxiangdong <phpwww@126.com>
2016-04-08 14:24:22 +0800

    Update Test.php
    修复Linux 环境下单元测试出错

--- framework/libs/Test.php
+++ framework/libs/Test.php
@@ -6,7 +6,7 @@ define('FRAMEWORK_PATH', dirname(__DIR__));
 //定义应用路径
 define('APP_PATH', dirname(FRAMEWORK_PATH).'/app');
 //引入worker初使化脚本
-require FRAMEWORK_PATH.'/bootstrap/initWorker.php';
+require FRAMEWORK_PATH.'/bootstrap/InitWorker.php';
 /**
  * 测试基类
  * Class Test
